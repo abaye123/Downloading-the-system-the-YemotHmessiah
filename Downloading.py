06b6ba08c22f6c3db0f043968bb932086b2b939c
abaye123 <46202069+abaye123@users.noreply.github.com>
2023-05-19 02:10:52 +0300

    Update Downloading.py
    * I added that it will also download the text files that accompany the audio files.
    * I added the option to select a file type to download from @amp-software-0

--- Downloading.py
+++ Downloading.py
@@ -14,6 +14,7 @@
 #   - Link: 
 ######################################################
 
+
 import requests
 import json
 import os
@@ -44,7 +45,6 @@ def download_file(url, destination):
 
 def process_item(item, parent_path):
     if item['fileType'] == 'DIR' or item['fileType'] == 'EXT':
-        # Create a subdirectory with the same name as the item
         dir_path = os.path.join(os.path.expanduser(
             '~'), new_folder_path, parent_path, item['name'])
         try:
@@ -53,7 +53,6 @@ def process_item(item, parent_path):
             print(f'Error creating directory {dir_path}: {str(e)}')
             return
 
-        # Fetch all items in the subdirectory
         response = requests.get(
             f'https://call2all.co.il/ym/api/GetIvrTree?path={item["what"]}&token={token}')
         try:
@@ -71,18 +70,32 @@ def process_item(item, parent_path):
             print(
                 f'Error fetching items for {item["what"]}: {data["responseStatus"]}')
     else:
-        file_url = f'https://call2all.co.il/ym/api/DownloadFile?path={item["what"]}&token={token}'
-        filename = item['name']
-        download_path = os.path.join(os.path.expanduser(
-            '~'), new_folder_path, parent_path, filename)
-        try:
-            download_file(file_url, download_path)
-            size_mb = format_file_size(item['size'])
-            print('\033[33mDownloaded:\033[0m', '\033[34m', filename, '\033[33mwhat\033[0m:',
-                  '\033[34m', item['what'], ' \033[33mSize\033[0m:', '\033[34m', size_mb, '\033[0m')
-        except Exception as e:
-            print(f'Error downloading file {filename}: {str(e)}')
-
+         if item['fileType'] == fileType or fileType == '':
+            if item['fileType'] ==  'AUDIO':
+                item_what = item["what"][:-3] + "txt"
+                filename = item['name'][:-3] + "txt"
+                file_url = f'https://call2all.co.il/ym/api/DownloadFile?path={item_what}&token={token}'
+                download_path = os.path.join(os.path.expanduser(
+                    '~'), new_folder_path, parent_path, filename)
+                try:
+                    download_file(file_url, download_path)
+                    size_mb = format_file_size(item['size'])
+                    print('\033[33mDownloaded:\033[0m', '\033[34m', filename, '\033[33mwhat\033[0m:',
+                        '\033[34m', item_what, ' \033[33mSize\033[0m:', '\033[34m', size_mb, '\033[0m')
+                except Exception as e:
+                    print(f'Error downloading file {filename}: {str(e)}')
+            
+            file_url = f'https://call2all.co.il/ym/api/DownloadFile?path={item["what"]}&token={token}'
+            filename = item['name']
+            download_path = os.path.join(os.path.expanduser(
+                '~'), new_folder_path, parent_path, filename)
+            try:
+                download_file(file_url, download_path)
+                size_mb = format_file_size(item['size'])
+                print('\033[33mDownloaded:\033[0m', '\033[34m', filename, '\033[33mwhat\033[0m:',
+                    '\033[34m', item['what'], ' \033[33mSize\033[0m:', '\033[34m', size_mb, '\033[0m')
+            except Exception as e:
+                print(f'Error downloading file {filename}: {str(e)}')
 
 while True:
     user = input('\033[33mהזן את מספר המערכת: \033[0m')
@@ -105,24 +118,29 @@ while True:
         print(f'Error fetching items: {str(e)}')
         print('\033[33mPlease try again:\033[0m\n')
 
-paths = input(
-    '\033[33mהזן נתיב הורדה מותאם אישית, בפורמט "2" או "9/1", להורדת המערכת כולה השאר ריק: \033[0m')
+paths = input('\033[33mהזן נתיב הורדה מותאם אישית, בפורמט "2" או "9/1", להורדת המערכת כולה השאר ריק: \033[0m')
+fileType = input('\033[33mהזן פורמט קובץ רצוי להורדה, יש להשתמש באותיות גדולות, להורדת כל סוגי הקבצים השאר ריק: \033[0m')
 
 root = tk.Tk()
 root.withdraw()
 folder_path = filedialog.askdirectory()
+
 if folder_path:
-    new_folder_path = folder_path + "/" + user + "/" + paths + "/"
+    new_folder_path = os.path.join(folder_path, user, paths)
     print("Selected folder path with: ", new_folder_path)
-    os.makedirs(new_folder_path)
+    
+    if not os.path.exists(new_folder_path):
+        os.makedirs(new_folder_path)
+    else:
+        print("Directory already exists.")
 else:
     print("No folder selected.")
 
 
 print(new_folder_path)
 print('Download starter...')
-response = requests.get(
-    f'https://call2all.co.il/ym/api/GetIvrTree?path=ivr2:{paths}/&token={token}')
+
+response = requests.get(f'https://call2all.co.il/ym/api/GetIvrTree?path=ivr2:{paths}/&token={token}')
 try:
     response.raise_for_status()
 except requests.exceptions.RequestException as e:
@@ -137,4 +155,5 @@ if data['responseStatus'] == 'OK':
         process_item(item, '')
 
 requests.get(f'https://call2all.co.il/ym/api/Logout?token={token}')
+
 print('\033[31mההורדה הושלמה בהצלחה!:\033[31m\n')
